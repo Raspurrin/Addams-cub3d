083f4c72a48315d11e963275c9b0420d379bae61
Anahit Kasamanyan <anikakasamanyan@gmail.com>
2023-01-18 20:15:46 +0100

    halp

--- srcs/engine/draw.c
+++ srcs/engine/draw.c
@@ -2,15 +2,7 @@
 
 void	move(t_data *data, bool up, bool dwn, bool rht, bool lft)
 {
-
-	// if (!(data->player.pos.y - 15 <= SCREEN_HEIGHT && data->player.pos.y - 15 > 0))
-	// 	return ;
-	// if (!(data->player.pos.y + 15 <= SCREEN_HEIGHT && data->player.pos.y + 15 > 0))
-	// 	return ;
-	// if (!(data->player.pos.x - 15 <= SCREEN_WIDTH && data->player.pos.x - 15 > 0))
-	// 	return ;
-	// if (!(data->player.pos.x + 15 <= SCREEN_WIDTH && data->player.pos.x + 15 > 0))
-	// 	return ;
+	// tem vec
 	if (up)
 		data->player.pos = vector_add(data->player.pos,  data->player.dir);
 	if (dwn)
@@ -19,25 +11,9 @@ void	move(t_data *data, bool up, bool dwn, bool rht, bool lft)
 		data->player.pos = vector_add(data->player.pos,  rotatevectorlol(data->player.dir, 90));
 	if (lft)
 		data->player.pos = vector_substr(data->player.pos,  rotatevectorlol(data->player.dir, 90));
+	// check if tmp in wall
+	// only if !wall update pos player
 }
-
-// void	move_down(t_data *data)
-// {
-	
-// 	// if (!(data->player.y + 15 <= SCREEN_HEIGHT && data->player.y + 15 > 0))
-// 	// 	return ;
-// 	data->player.pos.y += 15;
-// }
-
-// void	move_right(t_data *data)
-// {
-// 	data->player.pos.x += 15;
-// }
-
-// void	move_left(t_data *data)
-// {
-// 	data->player.pos.x -= 15;
-// }
 
 void	draw_addams_cube(t_data *data)
 {
@@ -50,17 +26,10 @@ void	draw_addams_cube(t_data *data)
 		y = 0;
 		while (y < 20)
 		{
-			// printf("the x that is the y: %i\nthe y that is the x: %i\n",x,y);
 			if (data->map[x][y] == '1')
-			{
-				// printf("zis is ze wall\n");
 				draw_the_walls(data, y*100, x*100, 1);
-			}
 			else
-			{
-				// printf("zis is not ze wall\n");
 				draw_the_walls(data, y*100, x*100, 0);
-			}
 			y++;
 		}
 		x++;
@@ -82,13 +51,8 @@ void	draw_the_walls(t_data *data, int x, int y, bool wall)
 		j = 0;
 		while (j < 100)
 		{
-			// printf("the i that is the x: %i\nthe j that is the y: %i\n",i,j);
 			if (wall)
-			{
-				// printf("zis is ze wall\n");
 				my_mlx_pixel_put(&data->img, i + x, j + y, 0xBA7CE7);
-				// printf("zis is ze wall afta mlx_pxl_put\n");
-			}
 			else
 				my_mlx_pixel_put(&data->img, i + x, j + y, 0x000000);
 			j++;
@@ -188,32 +152,6 @@ void draw_line_img(t_img *img, t_vector eins, t_vector zwei, int color)
 
 void	draw_the_vector(t_data *data)
 {
-	// int	x;
-	// int	y;
-	// t_vector map; // the /100 one 
-	// t_vector temp; // the *100 one
-	// temp.x = data->player.pos.y / TILE;  //3
-	// temp.y = data->player.pos.x / TILE;  //6
-		// printf("the x that is the y: %f\nthe y that is the x: %f\n",temp.x,temp.y);
-	// x = data->player.pos.y / TILE;
-	// y = data->player.pos.x / TILE;
-	// while (data->map[(int)temp.x][(int)temp.y] != '0' && data->map[(int)temp.x][(int)temp.y] != 'W')
-	// {
-	// 	temp.x = temp.x * TILE;  //300
-	// 	temp.y = temp.y * TILE;  //600
-	// 	temp = vector_add(temp, data->player.dir);
-	// 	temp.x = temp.x / TILE;  //3
-	// 	temp.y = temp.y / TILE;  //6
-	// }
-	// printf("%f %f\n", temp.x, temp.y);
-	// if (!(data->player.pos.y - DIR_VECTOR <= SCREEN_HEIGHT && data->player.pos.y - DIR_VECTOR > 0))
-	// 	return ;
-	// if (!(data->player.pos.y + DIR_VECTOR <= SCREEN_HEIGHT && data->player.pos.y + DIR_VECTOR > 0))
-	// 	return ;
-	// if (!(data->player.pos.x - DIR_VECTOR <= SCREEN_WIDTH && data->player.pos.x - DIR_VECTOR > 0))
-	// 	return ;
-	// if (!(data->player.pos.x + DIR_VECTOR <= SCREEN_WIDTH && data->player.pos.x + DIR_VECTOR > 0))
-	// 	return ;
 	draw_line_img(&data->img, data->player.pos, vector_add(data->player.dir, data->player.pos), 0x59D4F8);
 	
 }
--- srcs/main.c
+++ srcs/main.c
@@ -1,65 +1,16 @@
 #include "../includes/cub3d.h"
 
-// lol
-
-// void	init(t_data *data)
-// {
-// 	ft_bzero(data, sizeof(data));
-// 	ft_bzero(data->texture, sizeof(t_texture));
-// }
-
-// void	innit(t_data *data)
-// {
-
-	// char map[10][20] =	{"11111111111111111111",
-	// 					"1000P001100000001001",
-	// 					"10000001100000001001",
-	// 					"10011111100000111111",
-	// 					"10000000001110001101",
-	// 					"10000000011100001101",
-	// 					"10000000000000000001",
-	// 					"10001111000001000001",
-	// 					"10000000000011100001",
-	// 					"11111111111111111111"
-	// 				};
-
-	// 	int	x;
-	// int	y;
-
-// 	x = 0;
-// 	while (x < 10)
-// 	{
-// 		y = 0;
-// 		while (y < 20)
-// 		{
-// 			if (map[x][y] == 'P')
-// 			{
-// 				data->player.x = y * 100;
-// 				// printf("player x %d\n",data->player.x);
-// 				data->player.y = x * 100;
-// 				// printf("player y %d\n",data->player.y);
-// 			}
-// 			y++;
-// 		}
-// 		x++;
-// 	}
-// 	data->player.direction.x = 0;
-// 	data->player.direction.y = 10;
-// }
+//lol
 
 int32_t	main(int argc, char *argv[])
 {
 	t_data	data;
 
-	(void)argv;
-	(void)argc;
 	init(&data);
 	data.img.img_ptr = mlx_new_image(data.mlx,  SCREEN_WIDTH, SCREEN_HEIGHT);
 	data.img.data = mlx_get_data_addr(data.img.img_ptr,
 			&data.img.bpp, &data.img.size_l, &data.img.endian);
 	error_check(&data, argc, argv);
-	// printf("halp\n");
-	// print_2d_fd(data.map,1);
 	draw_addams_cube(&data);
 	mlx_hook(data.win, KEY_PRESS, 0, key_handler, &data);
 	mlx_hook(data.win, RED_CROSS, 0, x_close, &data);
