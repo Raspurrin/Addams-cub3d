82369de85d0ddf05cc8136af341fadf77fee8410
Anahit K <anikakasamanyan@gmail.com>
2023-02-16 19:37:13 +0100

    halp

---
+++ textures/ghost.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+ PURPLE='\033[0;34m'
+ RE='\033[0m'
+ BLACK='\033[30m'
+ WHITE_BG='\033[107m'
+ RED='\033[0;31m'
+ GREEN='\033[0;32m'
+ YELLOW='\033[0;33m'
+ PINK='\033[0;35m'
+ SKY='\033[0;36m'
+ BLUE='\033[034m'
+
+echo -e "                                                    "
+echo -e "                         ${PURPLE}XXXXXXXXX${RE}                      "
+echo -e "                      ${PURPLE}XXX${RE}         ))Y5X                 "
+# echo -e "                    ${PURPLE}XX${RE}               [T8%                "
+echo -e "                    ${PURPLE}XX${RE}                 [Tq%               "
+echo -e "                  ${PURPLE}XXX${RE}                    T88               "
+echo -e "                 ${PURPLE}XX${RE}                        ~Vq${PURPLE}%              "
+echo -e "                ${PURPLE}XX${RE}                          TTq${PURPLE}8              "
+echo -e "               ${PURPLE}XO${RE}                           [Tc${PURPLE}8              "
+echo -e "               ${PURPLE}X${RE}                             ~[J${PURPLE}8              "
+echo -e "              ${PURPLE}XX${RE}    XX                        T)K${PURPLE}8              "
+echo -e "              ${PURPLE}XO${RE}   XX 5        XXX            )Iq${PURPLE}5             "
+echo -e "              ${PURPLE}XX${RE}   XXx8       88 8X           )T8${PURPLE}5             "
+echo -e "              ${PURPLE}X${PINK} ydi${RE}XXX        XXX0X           )[8q             "
+echo -e "              ${PURPLE}X${PINK} pdd${RE}            XX%${PINK} dd${RE}          ]TN${PURPLE}8             "
+echo -e "              ${PURPLE}X${PINK} usd${RE}               ${PINK}ddddd${RE}        ][A${PURPLE}q             "
+echo -e "            ${PURPLE}XXX${RE}      ${RE}             ${PINK}dd ${RE}          T]F${PURPLE}8              "
+echo -e "           ${PURPLE}XX X${RE}                                []T${PURPLE}Kq             "
+echo -e "          ${PURPLE}XX  X${RE}                X%               T[${PURPLE}T8            "
+echo -e "          ${PURPLE}X  XX${RE}               X%                )]8${PURPLE}8            "
+echo -e "           ${PURPLE}XXX%${RE}              ;X        XX        T[${PURPLE}b9            "
+echo -e "              ${PURPLE}X${RE}               XX     XX          [~${PURPLE}D8            "
+echo -e "              ${PURPLE}X${RE}                XXX;X             [[${PURPLE}[t\            "
+echo -e "              ${PURPLE}8${RE}T                                  T[~${PURPLE}z\           "
+echo -e "              ${PURPLE}5${RE})                                  [[${PURPLE}T[zX            "
+echo -e "             ${PURPLE}g${RE}))                                   T[${PURPLE}[~8X            "
+echo -e "             ${PURPLE}z${RE}[)                                   )${PURPLE}TTzX            "
+echo -e "            ${PURPLE};${RE})))            )T                  [)${PURPLE})[RX             "
+echo -e "            ${PURPLE};z${RE})T)          )))T[T           [[[)X)T;X             "
+echo -e "              ${PURPLE};b${RE})))T[uhuufLG H~)) X     [T[Tzgj8X${PURPLE}XXX             "
+echo -e "               ${PURPLE}X5${RE}mR)Z888BL )999999))T[MX ${PURPLE}XXX                 "
+echo -e "                 ${PURPLE}O588 5988ST)YYY  T8X58XX  ${RE}                   "
+# echo -e "                             ${PURPLE}58XXXX                    "
+echo -e "                                                    "
